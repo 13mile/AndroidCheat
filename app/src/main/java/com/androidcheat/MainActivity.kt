e26414941c04600fc640d424db97b9a8db14b09c
chobeomseok <qjatjr1108@gmail.com>
2019-07-08 17:51:27 +0900

    Split module androidlheat
    Split module androidlheat

--- app/src/main/java/com/androidcheat/MainActivity.kt
+++ app/src/main/java/com/androidcheat/MainActivity.kt
@@ -4,12 +4,9 @@ import kotlinx.android.synthetic.main.activity_cheat.*
 
 class MainActivity : CheatBaseActivity() {
     override fun initializeMenus() {
-
         rightMenu.addText("cheat test")
         rightMenu.addButton("println"){
             println("hello world cheat android")
         }
-
     }
-
 }
