ceca5ca0e215937923cb7d95ce31a0a9e8b5d2fd
chobeomseok <qjatjr1108@gmail.com>
2019-07-09 18:41:20 +0900

    ver 0.0.19

--- androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
+++ androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
@@ -1,14 +1,10 @@
 package k.bs.androidcheat.cheat
 
-import android.app.Application
-import rx_activity_result2.RxActivityResult
-
 object Cheat {
     lateinit var initActivity: Class<*>
     lateinit var cheatActivity: Class<*>
-    inline fun <reified INIT_ACTIVITY, reified CHEAT_ACTIVITY> register(application: Application) {
+    inline fun <reified INIT_ACTIVITY, reified CHEAT_ACTIVITY> register() {
         initActivity = INIT_ACTIVITY::class.java
         cheatActivity = CHEAT_ACTIVITY::class.java
-        RxActivityResult.register(application)
     }
 }
--- androidcheat/src/main/java/k/bs/androidcheat/permission/GrantPermissionCheckManager.kt
+++ androidcheat/src/main/java/k/bs/androidcheat/permission/GrantPermissionCheckManager.kt
@@ -2,18 +2,23 @@ package k.bs.androidcheat.permission
 
 import android.content.Context
 import android.content.Intent
+import io.reactivex.Single
+import k.bs.androidcheat.R
 import k.bs.androidcheat.cheat.FloatingViewService
 import k.bs.androidcheat.rx.startActivityForResult
 import k.bs.androidcheat.rx.toIntent
 import k.bs.androidcheat.util.DontCare
 import k.bs.androidcheat.util.TLog
-import io.reactivex.Single
-import k.bs.androidcheat.BuildConfig
-import k.bs.androidcheat.R
+import kr.nextm.lib.AppInstance
 import kr.nextm.lib.TToast
+import rx_activity_result2.RxActivityResult
 
 object GrantPermissionCheckManager {
+    private fun init(){
+        RxActivityResult.register(AppInstance.get())
+    }
     fun check(context: Context) {
+        init()
         checkPermission(context)
             .subscribe({
                 showDebugInfoView(context)
@@ -27,6 +32,8 @@ object GrantPermissionCheckManager {
     }
 
     fun checkWithRx(context: Context): Single<DontCare> {
+        init()
+
         return checkPermission(context)
     }
 
