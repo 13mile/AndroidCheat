66a6beb8e511090f0f7645883a3c6bca2851d96b
chobeomseok <qjatjr1108@gmail.com>
2019-07-09 17:56:41 +0900

    appication class remove

--- androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
+++ androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
@@ -1,10 +1,14 @@
 package k.bs.androidcheat.cheat
 
+import kr.nextm.lib.AppInstance
+import rx_activity_result2.RxActivityResult
+
 object Cheat {
     lateinit var initActivity: Class<*>
     lateinit var cheatActivity: Class<*>
     inline fun <reified INIT_ACTIVITY, reified CHEAT_ACTIVITY> register() {
         initActivity = INIT_ACTIVITY::class.java
         cheatActivity = CHEAT_ACTIVITY::class.java
+        RxActivityResult.register(AppInstance.get())
     }
 }
