6232052396d92d6119ddbc62cd062e1b0446c799
chobeomseok <qjatjr1108@gmail.com>
2019-07-09 18:25:08 +0900

    ver 0.0.18

--- androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
+++ androidcheat/src/main/java/k/bs/androidcheat/cheat/Cheat.kt
@@ -1,14 +1,14 @@
 package k.bs.androidcheat.cheat
 
-import kr.nextm.lib.AppInstance
+import android.app.Application
 import rx_activity_result2.RxActivityResult
 
 object Cheat {
     lateinit var initActivity: Class<*>
     lateinit var cheatActivity: Class<*>
-    inline fun <reified INIT_ACTIVITY, reified CHEAT_ACTIVITY> register() {
+    inline fun <reified INIT_ACTIVITY, reified CHEAT_ACTIVITY> register(application: Application) {
         initActivity = INIT_ACTIVITY::class.java
         cheatActivity = CHEAT_ACTIVITY::class.java
-        RxActivityResult.register(AppInstance.get())
+        RxActivityResult.register(application)
     }
 }
