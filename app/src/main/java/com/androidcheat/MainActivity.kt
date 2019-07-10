0811370193a613e34c29cb3656e73a4f487c67d3
hyundh <hyundh@13mile.co.kr>
2019-07-11 00:55:20 +0900

    added import in MainActivity.kt

--- app/src/main/java/com/androidcheat/MainActivity.kt
+++ app/src/main/java/com/androidcheat/MainActivity.kt
@@ -2,10 +2,15 @@ package com.androidcheat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import k.bs.androidcheat.cheat.Cheat
+import k.bs.androidcheat.cheat.CheatActivity
+import k.bs.androidcheat.permission.GrantPermissionCheckManager
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Cheat.register<MainActivity,CheatActivity>()
+//        GrantPermissionCheckManager.check(this)
     }
 }
