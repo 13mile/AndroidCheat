3b277591f7a5b32f8728edb147d4d16fac42cae3
hyundh <hyundh@13mile.co.kr>
2019-07-10 23:21:28 +0900

    edited sample code

--- app/src/main/java/com/androidcheat/cheat/CheatActivity.kt
+++ app/src/main/java/com/androidcheat/cheat/CheatActivity.kt
@@ -5,6 +5,16 @@ import kotlinx.android.synthetic.main.activity_cheat.*
 
 class CheatActivity : CheatBaseActivity(){
      override fun initializeMenus() {
-         rightMenu.addButton("hellow cheat worl")
+         rightMenu.addButton("hellow cheat world"){
+             //Do Somting
+         }
+
+         bottomMenu.addButton("hellow bottom menu"){
+             //Do Somting
+         }
+
+         leftMenu.addButton("hellow left menu"){
+             //Do Somting
+         }
      }
  }
