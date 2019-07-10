be22796282936b429930f30bf5ebb04654efc796
hyundh <hyundh@13mile.co.kr>
2019-07-11 00:46:23 +0900

    clean line in MainActivity.kt

--- app/src/main/java/com/androidcheat/MainActivity.kt
+++ app/src/main/java/com/androidcheat/MainActivity.kt
@@ -3,10 +3,9 @@ package com.androidcheat
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 }
